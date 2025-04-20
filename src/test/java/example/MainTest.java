95f75672c06aecb2d953081d7b8a690cd8603d35
shixiongfei <xiongfei.shi@icloud.com>
2025-04-20 15:07:26 +0800

    Init Gradle project

---
+++ src/test/java/example/MainTest.java
@@ -0,0 +1,12 @@
+package example;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+    @Test
+    public void testMain() {
+        assertTrue(true);
+    }
+}
